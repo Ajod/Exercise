3690d401c4be0dd1239ce7600c0bdc36a0ab7235
Alexis <alexis.jodar@epitech.eu>
2019-07-08 12:09:28 +0200

    communication setup

---
+++ codegen.sh
@@ -0,0 +1,6 @@
+protoc -I/usr/local/include -I. \
+        --proto_path=proto \
+		-I${GOPATH}/src \
+		-I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+		--go_out=google/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. \
+		server.proto
